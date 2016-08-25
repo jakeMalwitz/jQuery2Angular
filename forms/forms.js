c0633d1a8162d30dce23b0f583e70d9525b9c961
Jake Malwitz <malwitzjake@comcast.net>
2016-08-25 16:20:24 -0500

    initial commit

--- forms/forms.js
+++ forms/forms.js
@@ -1,52 +1,29 @@
-$(document).ready(function() {
-    var array = [];
-    $('#employeeinfo').on('submit', function(event) {
-      event.preventDefault();
+var myApp = angular.module("myApp", []);
 
-      // initialize a new variable as an empty object
-      var values = {};
+myApp.controller("taskController", ["$scope", function($scope){
 
-      // convert the form inputs into an array
-      var fields = $('#employeeinfo').serializeArray();
+  $scope.total = 0;
+  $scope.emContent = [];
+  $scope.employee = {};
 
-      // iterate over the array and transfer each index into a new property on an object with the value of what was entered.
-      fields.forEach(function(element, index, array) {
-        // review index notation vs. dot notation on objects
-        // here, dot notation wouldn't work
-        values[element.name] = element.value;
-      });
+  $scope.getEmployee = function(){
+    console.log($scope.employee.emID);
+    if($scope.employee.emID !== undefined && $scope.employee.emYS !== NaN){
+    $scope.emContent.push($scope.employee);
+    $scope.total += $scope.employee.emYS;
+    $scope.employee = {};
+} else {
+  alert("Please enter blank fields");
+}
+};
 
-      console.log(values);
-
-      // clear out inputs
-      $('#employeeinfo').find('input[type=text]').val('');
-      $('#employeeinfo').find('input[type=number]').val('');
-      // append to DOM
-      appendDom(values);
-    });
-    var total = 0;
-    function appendDom(empInfo) {
-      $('#container').append('<div class="person"></div>');
-      var $el = $('#container').children().last();
-      $el.append('<p>' + empInfo.employeefirstname + ' ' + empInfo.employeelastname + ' ' + empInfo.employeeidnum + ' ' + empInfo.employeejobtitle + ' ' + empInfo.employeesalaryyearly + '<button>Remove Worker</button>' + '</p>');
-      var theValue = parseInt(($.data($('p').get(0),'EmployeeSalary', empInfo.employeesalaryyearly)));
-      if(isNaN(theValue)){
-        theValue = 0;
+  $scope.delete = function(id){
+    for (var i = 0; i < $scope.emContent.length; i++){
+      if($scope.emContent[i].emID == id){
+        $scope.total -= $scope.emContent[i].emYS;
+        $scope.emContent.splice(i, 1);
       }
-      total += theValue;
-      calculateTotal(total);
-      $el.on('click', function(){
-      removeDom(this, theValue);
-      });
-    }
+    };
+  };
 
-    function removeDom(thing, otherThing) {
-      total -= otherThing;
-      $(thing).remove();
-      calculateTotal(total);
-    }
-
-    function calculateTotal(totalA) {
-      $('#monthlySalary').children().text("Total Monthly Salary: " + (totalA/12).toFixed(2));
-    }
-});
+}]);
